b98dc95ab5ee139e87e379a8e795e9aecca3022f
David Mendoza <115253293+vhatara@users.noreply.github.com>
2023-02-26 15:27:14 +0800

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300 ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
